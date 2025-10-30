9238ec0f0e0107ccf2cad1bff2aacf63ee9e180d
AdamTB10 <agtro06@gmail.com>
2025-10-30 14:52:51 +1100

    Week 7 - Activity 1

--- Week7/Activity1/main.cpp
+++ Week7/Activity1/main.cpp
@@ -1,3 +1,29 @@
 /*
-
+    This program will have a function that finds the minimum number from a user input of three numbers.
 */
+
+#include <iostream>
+using namespace std;
+
+int findMinimum(int num1, int num2, int num3) {
+    int minNum = num1;
+
+    if (num2 < minNum) {
+        minNum = num2;
+    }
+    if (num3 < minNum) {
+        minNum = num3;
+    }
+
+    return minNum;
+}
+
+int main() {
+    int num1, num2, num3;
+    cout << "Please enter 3 numbers: ";
+    cin >> num1 >> num2 >> num3;
+
+    cout << "Minimum number is: " << findMinimum(num1, num2, num3) << endl;
+    
+    return 0;
+}
